0eae1c113b8d5314273636a953ecca15c5e6f66e
sravanisabbisett <sravani.sabbisetti1@gmail.com>
2020-12-05 19:27:24 +0530

    STARTED:Maximum Test

---
+++ FindMaximum/Maximum.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FindMaximum
+{
+    class Maximum
+    {
+        public int maximumInteger(int x,int y,int z)
+        {
+            int maximum = x;
+            if (y.CompareTo(maximum) > 0)
+                maximum = y;
+            if (z.CompareTo(maximum) > 0)
+                maximum = z;
+            return maximum;
+        }
+
+        public double MaximumDouble(double x,double y,double z)
+        {
+            double maximum = x;
+            if (y.CompareTo(maximum) > 0)
+                maximum = y;
+            if (z.CompareTo(maximum) > 0)
+                maximum = z;
+            return maximum;
+        }
+    }
+}
---
+++ FindMaximum/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace FindMaximum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            /*Maximum maximum = new Maximum();
+            int maxInt=maximum.maximumInteger(5, 7, 9);
+            Console.WriteLine("Maximum of three Integers:"+maxInt);
+            double maxDouble = maximum.MaximumDouble(8.8, 9.9, 10.0);
+            Console.WriteLine("Maximum of three doubles:" + maxDouble);*/
+            int[] arr = { 114, 153, 144, 110 };
+            Geneiric<int> geneiric = new Geneiric<int>(arr);
+            geneiric.Print();
+            Console.ReadLine();
+        }
+    }
+}
